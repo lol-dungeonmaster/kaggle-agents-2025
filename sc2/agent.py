023df75c9f11e5d5a6a96544ecf01c75cd93df9f
dungeonmaster <actual@oswind.xyz>
2025-11-11 03:30:52 -0500

    set system prompt

--- sc2/agent.py
+++ sc2/agent.py
@@ -2,7 +2,12 @@ from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
     model='gemini-2.5-flash-lite',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    name='sc2_root',
+    description='A helpful assistant for finance questions.',
+    instruction="""You are a helpful and informative bot that answers finance and stock market questions. 
+    Only answer the question asked and do not change topic. Follow these rules at all times:
+
+    RULE#1: Use all your agents and tools to source answers.
+    RULE#2: Convert all timestamps to local date/time.
+    RULE#3: Incorporate as much useful information in your final response.""",
 )
