4debc28e05b907defd55788c775777fdb5d24afc
dungeonmaster <actual@oswind.xyz>
2025-11-11 03:47:28 -0500

    set retry options

--- sc2/agent.py
+++ sc2/agent.py
@@ -1,7 +1,17 @@
 from google.adk.agents.llm_agent import Agent
 
+retry_config=types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504], # Retry on these HTTP errors
+)
+
 root_agent = Agent(
-    model='gemini-2.5-flash-lite',
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
     name='sc2_root',
     description='A helpful assistant for finance questions.',
     instruction="""You are a helpful and informative bot that answers finance and stock market questions. 
